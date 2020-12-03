311fcb65cfe8445d68cf26cbe1797771a2bbb3cd
Tomas Elias <tomas.elias.za@gmail.com>
2020-12-03 21:19:15 +0100

    Wip 2020-03

---
+++ zdroj/den03.c
@@ -0,0 +1,46 @@
+#include "../knihovna/czech.h"
+#include "../knihovna/helper.h"
+#include <stdint.h>
+#include <stdio.h>
+
+#define DEN "03"
+
+#define DÉLKA 323
+#define ŠÍŘKA 32
+#define STROM '#'
+
+číslo64_t počet_stromů(znak les[DÉLKA][ŠÍŘKA], číslo x, číslo y) {
+    číslo64_t počet_stromů = 0;
+
+    číslo souřadnice_x = 0;
+    číslo souřadnice_y = 0;
+
+    dokud(souřadnice_y < DÉLKA) {
+        počet_stromů += les[souřadnice_y][souřadnice_x] == STROM;
+
+        souřadnice_x = (souřadnice_x + x) % ŠÍŘKA;
+        souřadnice_y += y;
+    }
+
+    vrať počet_stromů;
+}
+
+číslo hlavní(prázdno) {
+    znak les[DÉLKA][ŠÍŘKA];
+
+    pro(velikost_t i = 0; i < DÉLKA; ++i) {
+        čtif("%s", les[i]);
+    }
+
+    číslo x[5][2] = { { 1, 1 }, { 3, 1 }, { 5, 1 }, { 7, 1 }, { 1, 2 } };
+
+    číslo64_t první_část = počet_stromů(les, x[1][0], x[1][1]);
+
+    číslo64_t druhá_část = 1;
+    pro(velikost_t i = 0; i < 5; i++) {
+        druhá_část *= počet_stromů(les, x[i][0], x[i][1]);
+    }
+
+    tiskni_řešení(DEN, první_část, druhá_část);
+    vrať 0;
+}
